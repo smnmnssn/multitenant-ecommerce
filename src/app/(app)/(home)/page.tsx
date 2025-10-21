042d5938030e6cc8267902046a39691604b0f3a8
smnmnssn <simonmansson@live.se>
2025-10-21 12:07:44 +0200

    payload integration

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,19 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+
+export default async function Home() {
+  const payload = await getPayload({
+      config: configPromise,
+    })
+  
+    const data = await payload.find({
+      collection: 'categories',
+    })
+
+  return (
+    <div className="flex flex-col gap-y-4 p-4">
+      {JSON.stringify(data, null, 2)}
+
+    </div>
+  );
+}
